473485380f280a4afd0c566d19f156d3c423b973
Augists PC <augists-pc@duck.com>
2023-10-27 17:12:58 +0800

    hack bilibili api and test

--- update/update_video_list.py
+++ update/update_video_list.py
@@ -1,15 +1,16 @@
 import requests
 import json
+import uuid
+import random
 import datetime
 import os
 from bs4 import BeautifulSoup as bs
 
 HEADERS = {
     'authority': 'api.bilibili.com',
-    'accept': 'application/json, text/plain, */*',
+    # 'path': '/x/space/wbi/arc/search?mid=1637070460&pn=1&ps=25&index=1&order=pubdate&order_avoided=true&platform=web&web_location=1550101&w_rid=9f20eea6cf38eadbb274ce17de811a7e&wts=1698396618',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
     'accept-language': 'zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
-    'origin': 'https://space.bilibili.com',
-    'referer': 'https://space.bilibili.com/489667127/channel/collectiondetail?sid=249279',
     'sec-ch-ua': '"Chromium";v="106", "Microsoft Edge";v="106", "Not;A=Brand";v="99"',
     'sec-ch-ua-mobile': '?0',
     'sec-ch-ua-platform': '"Windows"',
@@ -17,6 +18,10 @@ HEADERS = {
     'sec-fetch-mode': 'cors',
     'sec-fetch-site': 'same-site',
     'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36 Edg/106.0.1370.37',
+}
+
+COOKIES = {
+    'buvid3': "{}{:05d}infoc".format(uuid.uuid4(), random.randint(1, 99999))
 }
 
 
@@ -51,8 +56,8 @@ class video_info_list:
             os.mknod(self.json_path)
             return
         with open(self.json_path, 'r', encoding='utf-8') as f:
-            video_list_json = json.load(f)
-            for video_info_json in video_list_json:
+            load_video_list_json = json.load(f)
+            for video_info_json in load_video_list_json:
                 self.video_list.append(video_info(video_info_json))
 
     def sort_video_info(self):
@@ -60,7 +65,7 @@ class video_info_list:
 
     def get_latest(self) -> video_info:
         self.sort_video_info()
-        return self.video_list[-1]
+        return self.video_list[-1] if len(self.video_list) else None
 
 
 def update_video_list(up_url: str) -> None:
@@ -72,7 +77,7 @@ def update_video_list(up_url: str) -> None:
 
     video_list = video_info_list(json_file)
     latest_video = video_list.get_latest()
-    if latest_video.url != get_latest_video(up_url):
+    if latest_video == None or latest_video.url != get_latest_video(up_url):
         video_list = get_all_video(up_url)
         write_json(video_list, json_file)
 
@@ -82,11 +87,14 @@ def get_latest_video(url: str) -> str:
 
 
 def get_all_video(url: str) -> video_info_list:
-    res = requests.get(url, headers=HEADERS)
+    res = requests.get(url, headers=HEADERS, cookies=COOKIES)
     res.encoding = 'utf-8'
+    print(res.text)
     soup = bs(res.text)
+    # print(soup)
     BVselector = soup.select_one("div.section.video.full-rows")
-    for BV in BVselector.select('')
+    # for BV in BVselector.select('div'):
+    # print(BV)
     return video_info_list()
 
 
@@ -97,5 +105,9 @@ def write_json(video_list: video_info_list, json_file: str) -> None:
 
 if __name__ == '__main__':
     up_id = '1637070460'
-    up_url = 'https://space.bilibili.com/' + up_id
-    update_video_list(up_url)
+    # path: /x/space/wbi/arc/search?mid=1637070460&ps=30&tid=0&pn=1&keyword=&order=pubdate&platform=web&web_location=1550101&order_avoided=true&w_rid=5f623e925753e2a2c08c9820ada9a3b9&wts=1698397697
+    # path: /x/space/wbi/arc/search?mid=1637070460&ps=30&tid=0&pn=6&keyword=&order=pubdate&platform=web&web_location=1550101&order_avoided=true&w_rid=5763958a6ea4eb77e4f6020ef4fa5814&wts=1698397854
+    up_url = 'https://api.bilibili.com/x/space/wbi/arc/search?mid=' + \
+        up_id + '&order=pubdate&order_avoided=true&platform=web'
+    # update_video_list(up_url)
+    print(get_all_video(up_url))
